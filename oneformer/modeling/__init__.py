fab4a2b0a75f52d5ea59a58bc4cf4bc25e0d14a3
meehirmhatrepy <66881325+meehirmhatrepy@users.noreply.github.com>
2025-05-14 15:04:54 +0100

    oneformer
    first commit

---
+++ oneformer/modeling/__init__.py
@@ -0,0 +1,7 @@
+from .backbone.swin import D2SwinTransformer
+from .backbone.dinat import D2DiNAT
+from .backbone.fastervit import FasterViTransformer
+from .backbone.convnext import D2ConvNeXt
+from .pixel_decoder.fpn import BasePixelDecoder
+from .pixel_decoder.msdeformattn import MSDeformAttnPixelDecoder
+from .meta_arch.oneformer_head import OneFormerHead
